1a81db4865bf0f79e395211ab22d5e183b6606e4
Salmon Joy <salmonjoy1997@gmail.com>
2024-06-08 22:23:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$("button").click(function(){
+
+    let l1 = parseInt($('#floatingInputLike1').val())+parseInt($('#floatingInputLike2').val())+parseInt($('#floatingInputLike3').val())+parseInt($('#floatingInputLike4').val())
+    alert(l1);
+
+  });
