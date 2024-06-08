3a506725c7211bc0272dc1385dc2c5105b8990df
Salmon Joy <salmonjoy1997@gmail.com>
2024-06-08 23:06:17 +0530

    total points calculated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $("button").click(function(){
 
-    let l1 = parseInt($('#floatingInputLike1').val())+parseInt($('#floatingInputLike2').val())+parseInt($('#floatingInputLike3').val())+parseInt($('#floatingInputLike4').val())
-    alert(l1);
+    let totalViews = parseInt($('#floatingInputViews1').val())+parseInt($('#floatingInputViews2').val())+parseInt($('#floatingInputViews3').val())
+    let totalLikes = parseInt($('#floatingInputLikes1').val())+parseInt($('#floatingInputLikes2').val())+parseInt($('#floatingInputLikes3').val())
+    let totalComments = parseInt($('#floatingInputComments1').val())+parseInt($('#floatingInputComments2').val())+parseInt($('#floatingInputComments3').val())
+    let totalShares = parseInt($('#floatingInputShares1').val())+parseInt($('#floatingInputShares2').val())+parseInt($('#floatingInputShares3').val())
+    let points = totalViews+totalLikes/5+totalComments*7+totalShares*2
+    let bonusPoints = 500*(parseInt(parseInt($('#floatingInputViews1').val())/5000)+parseInt(parseInt($('#floatingInputViews2').val())/5000)+parseInt(parseInt($('#floatingInputViews3').val())/5000))
+    let totalPoints = points + bonusPoints;
+    $("#totalViews").append(totalViews);
+    $("#totalLikes").append(totalLikes);
+    $("#totalComments").append(totalComments);
+    $("#totalShares").append(totalShares);
+    $("#totalPoints").append(totalPoints);
+
 
   });
